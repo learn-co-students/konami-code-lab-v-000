d420e08bf0c4c0de800897136eb1422b42bbbdc0
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-22 18:50:09 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+
+// var correct_value = 10
+// var current_value = 0
+
+const main = document.querySelector('p')
+ 
+main.addEventListener('click', function(event) {
+  return alert('I was clicked!')
+  })
+
+
+
 }
+
+

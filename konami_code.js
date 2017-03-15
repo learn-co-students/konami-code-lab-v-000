b7f3bc3e979b6e99ddf8beb05abe00dbbf771599
Christian Whitesides <whitesidescr@gmail.com>
2017-03-15 16:57:02 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// [up arrow, up arrow, down arrow, down arrow, left arrow, right arrow, left arrow, right arrow, B, A]
 
 function init() {
   var index = 0

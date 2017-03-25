bb02b301475904c17eb692f5222a2f3faf81433c
Pedro Acosta <pedro.acosta613@gmail.com>
2017-03-25 02:00:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
 
 function init() {
-  // your code here
+  const body = document.body
+
+  body.addEventListener("keydown", function(e){
+    var key = parseInt(e.detail || e.which)
+
+    if (key === code[index]){
+      index++
+
+      if (index === code.length -1) {
+        alert("Right code is entered")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  });
 }
+
+// init()

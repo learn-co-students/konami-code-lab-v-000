05fde5d6b7ca2c6ac2ee63b847c2eed348157c97
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-10 16:54:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which) 
+
+  if (key === code[index]) {
+    console.log(e.which)
+    index++
+
+    if (index === code.length) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}
+ 
 function init() {
   // your code here
+  const body = document.body 
+
+  body.addEventListener('keydown', onKeyDownHandler)
+
 }

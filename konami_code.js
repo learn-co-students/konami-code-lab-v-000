42676238d175a6fce3389fd08db33cd1ebac0c42
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-26 15:49:35 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
 
 function init() {
-  // your code here
+    document.addEventListener('keydown',onKeyDownHandler)
+}
+
+function onKeyDownHandler(e) {
+
+    const key = parseInt(e.location)
+    console.log(String(key))
+//    debugger
+    if (key === code[index]){
+        index++
+
+        if (index === code.length - 1) {
+            alert("You did it")
+            index = 0
+        }
+
+    }else{
+        index = 0
+    }
 }

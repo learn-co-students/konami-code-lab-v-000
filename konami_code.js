f444b5b5e14fa23748830276628f3cf41442fe6a
Alex Glover <Alex@MacBook-Pro-3.local>
2017-04-27 12:23:22 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const body = document.querySelector('body')
+
 function init() {
-  // your code here
+  var index = 0
+
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++
+      if (index === code.length - 1) {
+        alert("AW YEAH YOU DID IT UH HUHHHHH WOOOO YEAHHH BOI!!!!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
 }

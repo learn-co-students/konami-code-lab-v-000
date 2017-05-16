4693cc71c0ad5ef662efcf84ee82226d44b7f45d
kkterai <kkterai@gmail.com>
2017-05-16 15:25:11 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const keys = []
 
-const body = document.querySelectorAll('body')
-
-body.addEventListener('keydown', function(e) {
+body.addEventListener('keydown', init);
     // keys.push(e.key)
     // for (let i = 0; i < code.length; i++)
     //   if (keys[i] !== code[i]) {
     //     return "Sorry, try again"
     //   }
+function init() {
     i = 0
     while (i < code.length) {
     if e.key === code[i]
     i += 1
     }
-    function init()
-  }
-  return "Sorry - Try again."
-}
-
-function init() {
   alert('Success! - You have broken the Konami Code!')
-}
+  }

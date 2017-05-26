1ffd3e0ea6be4462a7767d069c438c47ed1be53a
Trai Compton <tlcompton2546@outlook.com>
2017-05-26 18:19:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var i = 0
+
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[i]) {
+      i++
+
+      if (i === code.length - 1) {
+        alert('Amen !! Do your thing !!')
+        i = 0
+      }
+    } else {
+      i = 0
+    }
+  })
 }

decc7c408b98254fca499ef885092eff9fe21432
wook kim <wooke2e@gmail.com>
2017-06-07 16:20:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+function init(e) {
+  const key = parseInt(e.detail || e.which)
+
+  if (key === code)
 }

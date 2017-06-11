71c5cae42ec790d1e1563f2f85f3375c10b44bd2
Seth <itz.saga@gmail.com>
2017-06-11 17:02:30 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  debugger;
   let index = 0
   const doc = document.body
   doc.addEventListener('keydown', function onKeyDownHandler(e) {

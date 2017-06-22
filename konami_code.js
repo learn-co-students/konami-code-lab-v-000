85ef589f85f6c41c75c0f8e21cfe8b13a9a7003b
Howard DeVennish <howarddevennish@gmail.com>
2017-06-22 03:24:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const body = document.getElementsByTagName('body')
+
+body.addEventListener('click', function(event){
+  alert('I was clickered!')
+})
+
+
 function init() {
-  // your code here
+
 }

9a20987fbdce85cc02b9b158b6702861337ba25f
Howard DeVennish <howarddevennish@gmail.com>
2017-06-22 14:57:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,22 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const bod = document.body
-var clickArray = []
-
+var index = 0
 function init() {
-  bod.addEventListener('keydown', function(e){
-    clickArray.push(e.which)
-    if (arraysEqual(code.slice(-10), clickArray.slice(-10))) {
-      window.alert('You entered the Konami Code!')
-      console.log('You entered the Konami Code!')
+  document.body.addEventListener('keydown', function (e){
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length - 1) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    } else {
+      index = 0
     }
   })
-}
-
-function arraysEqual(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] != arr2[i]) {
-      return false
-    }
-  }
-  return true
 }

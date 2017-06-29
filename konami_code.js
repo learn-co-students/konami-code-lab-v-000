8e8b19b6351f67061eac6446e7d39ea82c21ab6b
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-06-29 17:59:12 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+     // your code here
+     const input = document.querySelector('input')
+
+     input.addEventListener('keydown', function(e) {
+          console.log(e.which)
+     })
 }

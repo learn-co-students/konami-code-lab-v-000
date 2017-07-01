9a3636a270e5304af1f8003dac78ebd8f7b86ff9
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-07-01 00:21:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 
 function init() {
-
-     const input = document.querySelector('input')
-
-     input.addEventListener('keydown', function(e) {
-          console.log(e.which)
-     })
-
+     var i = 0;
+     document.body.addEventListener('keydown', function(e) {
+          const key = parseInt(e.detail || e.which)
+          if (key == code[i])
+               i++;
+          else
+               i = 0;
+          if (i == code.length) {
+               alert('Konami!');
+          }
+     });
 }
+
+init();

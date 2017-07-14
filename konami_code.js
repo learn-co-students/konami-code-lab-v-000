5af6f353a305cb1dfeaa217440fc49692fccc339
lucketta <lucketta@umich.edu>
2017-07-13 23:55:22 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0
 
+function init() {
+  const body = document.querySelectorAll('body');
+
+  body.addEventListener('keydown', triggerKeys(e) {
+  })
 }
+//
+// function triggerKeys(e) {
+//   const key = parseInt(e.detail || e.which)
+//
+//   if (key === code[index]) {
+//     index++;
+//
+//     if (index === code.length) {
+//       alert("YAY!");
+//     }
+//   } else {
+//     index = 0
+//   }
+// }

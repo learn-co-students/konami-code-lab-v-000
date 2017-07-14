8c37146ec82cd9eb3e25c92dac2b3c8b32a86a16
lucketta <lucketta@umich.edu>
2017-07-13 23:58:03 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0
-
 function init() {
-  const body = document.querySelectorAll('body');
 
-  body.addEventListener('keydown', triggerKeys(e) {
-  })
+  let index = 0
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("YAY!");
+      }
+    } else {
+      index = 0
+    }
+  });
 }
-//
-// function triggerKeys(e) {
-//   const key = parseInt(e.detail || e.which)
-//
-//   if (key === code[index]) {
-//     index++;
-//
-//     if (index === code.length) {
-//       alert("YAY!");
-//     }
-//   } else {
-//     index = 0
-//   }
-// }

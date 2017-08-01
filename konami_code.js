ff3f620dc05bd98e815c024ee7b50374b3b33226
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-08-01 12:42:46 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  var index = 0;
+  document.addEventListener("keydown", konamiCode)
+  function konamiCode(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index ++;
+      if (index === code.length) {
+        alert("Wow. Such Secret. So Konami. Wow.");
+        index = 0;
+      }
+    } else {
+      index = 0;
 
+    }
+  }
 }

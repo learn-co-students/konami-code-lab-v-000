69584317c253a145cb4ac3afbdf5ab2dadd13594
Damien de Lartigue <damien@delartigue.co.uk>
2017-08-18 00:16:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  var counter = 0
 
+
+  document.body.addEventListener('keydown', function(e) {
+    if (parseInt(e.which) === code[counter] || parseInt(e.detail) === code[counter] || parseInt(e.location) === code[counter]) {
+      if(counter === code.length -1) {
+        alert("Well done")
+        counter = 0
+      }
+      counter++
+    } else {
+      counter = 0
+    }
+  })
 }

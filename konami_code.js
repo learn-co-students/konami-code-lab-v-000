c6cb77bb02d9a68991bd1dee65069a2c6732418c
Jdville03 <jarrel.delottinville@gmail.com>
2017-09-18 22:13:49 -0400

    refactored

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0
-  function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which)
     if (key === code[index]) {
       index++
@@ -14,6 +14,5 @@ function init() {
     } else {
       index = 0
     }
-  }
-  document.body.addEventListener('keydown', onKeyDownHandler)
+  })
 }

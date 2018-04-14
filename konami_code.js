4019f20e5fa7a656a21946b75f7977a5b5dfd02e
smithwebtek <brad@smithwebtek.com>
2018-04-13 23:43:57 -0400

    Show 2 ways to solve

--- konami_code.js
+++ konami_code.js
@@ -1,45 +1,46 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-// function init() {
-//   var index = 0;
-
-//   document.body.addEventListener('keydown', function(e) {
-//     const key = parseInt(e.which || e.detail);
-//     if (code[index] === key) {
-//       index++;
-
-//       if (index === code.length - 1) {
-//         alert('hurray!');
-
-//         index = 0;
-//       }
-//     } else {
-//       index = 0;
-//     }
-//   });
-// }
-
-
 var main = document.body;
 
 function init() {
-  document.addEventListener('keydown', function(e) {
-    for (let i = 0, l = code.length; i < l; i++) {
-    if (parseInt(e.keyCode) || parseInt(e.detail) === parseInt(code[i])){      
-      code.shift();
-      if (i === code.length) {
-      window.alert("KONAMI CODE CRACKED init");
-        codeCheck();
-        }
-      }
-    }
-  });
+	document.addEventListener('keydown', function (e) {
+		for (let i = 0, l = code.length; i < l; i++) {
+			if (parseInt(e.keyCode) || parseInt(e.detail) === parseInt(code[i])) {
+				code.shift();
+				if (i === code.length) {
+					window.alert("KONAMI CODE CRACKED init");
+					codeCheck();
+				}
+			}
+		}
+	});
 }
 
-function codeCheck(){
-  if (code.length === 0) {
-    alert("KONAMI CODE CRACKED codeCheck");
-  }
+function codeCheck() {
+	if (code.length === 0) {
+		alert("KONAMI CODE CRACKED codeCheck");
+	}
 }
+
+
+// function init() {
+// 	let index = 0;
+
+// 	document.body.addEventListener('keydown', function (e) {
+// 		const key = parseInt(e.which || e.detail);
+// 		console.log(key);
+
+// 		if (code[index] === key) {
+// 			index++;
+
+// 			if (index === code.length) {
+// 				alert('Hurray!');
+
+// 				index = 0;
+// 			}
+// 		} else {
+// 			index = 0;
+// 		}
+// 	});
+// }
 
 init();
